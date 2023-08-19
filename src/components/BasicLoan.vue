782cf03c5b0c09eb94ce914f5452f4e13bfd9ab2
DaveBruwer <davebruwer@gmail.com>
2023-08-19 21:11:10 +1200

    some basic first steps

---
+++ src/components/BasicLoan.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Super Barnacle!!!</h1>
+  </div>
+  <form action="">
+    <label for="loanAmount">Loan Amount:</label>
+    <input type="number">
+  </form>
+</template>
+
+<script setup>
+  
+</script>
+
+<style>
+  
+</style>
