0f6ea3b5155e43437d492f63570b682f641fd20b
DaveBruwer <davebruwer@gmail.com>
2023-08-24 21:40:50 +1200

    changed from refs to a singel reactive object

--- src/views/BasicLoanView.vue
+++ src/views/BasicLoanView.vue
@@ -2,28 +2,28 @@
   <div style="display: flex; flex-direction: column; padding: 1em;">
     <div style="display: flex; justify-content: space-around;">
       <div>
-        <label for="loanAmount">Loan Amount: {{currency}}</label>
-        <input id="loanAmount" type="number" v-model="loanAmount" style="width: 7em;">
+        <label for="loanAmount">Loan Amount: {{bond.currency}}</label>
+        <input id="loanAmount" type="number" min="1" v-model="bond.loanAmount" style="width: 7em;">
       </div>
       <div>
         <label for="interestRate">Interest Rate: </label>
-        <input id="interestRate" type="number" min="1" max="100" v-model="interestRate" style="width: 3em; text-align: end;">
+        <input id="interestRate" type="number" min="1" max="100" v-model="bond.interestRate" style="width: 3em; text-align: end;">
         <span>%</span>
       </div>
       <div>
         <label for="loanPeriod">Loan Period: </label>
-        <input id="loanPeriod" type="number" min="1" max="720" v-model="loanPeriod" style="width: 3em; text-align: end;">
+        <input id="loanPeriod" type="number" min="1" max="720" v-model="bond.loanPeriod" style="width: 3em; text-align: end;">
         <span>Years</span>
       </div>
     </div>
     <div style="display: flex; justify-content: space-around;">
       <div>
-        <label for="MinPayment">Min Monthly Payment: {{currency}}</label>
-        <span>{{ minPayment }}</span>
+        <label for="MinPayment">Min Monthly Payment: {{bond.currency}}</label>
+        <span>{{ bond.minPayment }}</span>
       </div>
       <div>
-        <label for="ActPayment">Actual Monthly Payment: {{currency}}</label>
-        <input id="interestRate" type="number" :placeholder="minPayment" v-model="actualPayment">
+        <label for="ActPayment">Actual Monthly Payment: {{bond.currency}}</label>
+        <input id="interestRate" type="number" min="1" :placeholder="bond.minPayment" v-model="bond.actualPayment">
       </div>
     </div>
     <div>
@@ -39,42 +39,75 @@
       <span>$1000000</span>
     </div>
   </div>
+  <table>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+      <th>5</th>
+      <th>6</th>
+      <th>7</th>
+      <th>8</th>
+      <th>9</th>
+      <th>10</th>
+      <th>11</th>
+      <th>12</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>
+        <input type="number">
+        <div>something else</div>
+      </td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+  </table>
 </template>
 
 <script setup>
-import {ref, reactive, computed} from 'vue'
-
-const currency = ref("$")
+import {ref, reactive, computed, watch} from 'vue'
 
 const currencyFormatter = new Intl.NumberFormat('en-US')
 
-let loanAmount = ref(48000)
-
-let interestRate = ref(1)
-
-let loanPeriod = ref(5)
-
-let actualPayment = ref()
-
-let monthlyInterest = computed(() => {
-  return interestRate.value/1200
+const bond = reactive({
+  currency: "$",
+  loanAmount: 48000,
+  interestRate: 1,
+  loanPeriod: 5,
+  actualPayment: null,
+  monthlyInterest:  computed(() => {
+    return bond.interestRate/1200
+  }),
+  periodInMonths: computed(() => {
+    return bond.loanPeriod*12
+  }),
+  minPayment: computed(() => {
+    const minPayment_ = (bond.loanAmount*bond.monthlyInterest)/(1-1/((1+bond.monthlyInterest)**bond.periodInMonths))
+    return currencyFormatter.format(minPayment_)
+  }),
 })
 
-let periodInMonths = computed(() => {
-  return loanPeriod.value*12
-})
-
-let minPayment = computed(() => {
-  const minPayment_ = (loanAmount.value*monthlyInterest.value)/(1-1/((1+monthlyInterest.value)**periodInMonths.value))
-  return currencyFormatter.format(minPayment_)
-})
-
-
-
-  
 </script>
 
 <style scoped>
+
+th {
+  border-bottom: 1px solid black;
+}
+
+td {
+  border-bottom: 1px solid black;
+}
+
+tr {
+  border: 1px solid red;
+}
 
 
   
