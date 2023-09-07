4eb701811626a96efae31d02e2e4bda4694b1599
DaveBruwer <davebruwer@gmail.com>
2023-09-07 21:41:44 +1200

    FINALLY got basic chart.js functionality!

--- src/views/BasicLoanView.vue
+++ src/views/BasicLoanView.vue
@@ -40,6 +40,7 @@
     </div>
   </div>
   <!-- <button @click="buttonPress">Press Me!</button> -->
+  <canvas id="myChart"></canvas>
   <!-- <div>______________________________________________</div>
   <table>
     <thead>
@@ -152,7 +153,32 @@
 </template>
 
 <script setup>
-import {ref, reactive, computed, watch, watchEffect} from 'vue'
+import {ref, reactive, computed, watch, watchEffect, onMounted} from 'vue'
+import Chart from 'chart.js/auto'
+
+onMounted(() => {
+  const ctx = document.getElementById('myChart');
+  
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+})
+
 
 
 const currencyFormatter = new Intl.NumberFormat('en-US')
