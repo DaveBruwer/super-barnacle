4733fe2e5ccb55facff7b0e8093364671977c3f6
DaveBruwer <davebruwer@gmail.com>
2023-10-19 20:11:57 +1300

    stripped vuetify, it sucks!

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,20 +1,8 @@
 <template>
-  <v-app :theme="darkMode ? 'dark' : 'light'">
-    <v-app-bar title="Super Barnacle">
-      <v-switch v-model="darkMode" label="Dark Mode"></v-switch>
-    </v-app-bar>
-    <v-main>V Main Content</v-main>
-  </v-app>  
+  WTF
 </template>
 
 <script setup>
-import {ref} from 'vue'
-
-const theme = ref('dark')
-
-const darkMode = ref(true)
-
-console.log(darkMode.value)
   
 </script>
 
