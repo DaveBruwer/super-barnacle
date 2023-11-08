c99a8b100069628e83647e69465e70c4ea22bc49
DaveBruwer <davebruwer@gmail.com>
2023-11-08 21:32:40 +1300

    implemented primevue Dialog modals

--- src/views/BasicLoanView.vue
+++ src/views/BasicLoanView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" m-2 flex justify-content-center align-content-center">
-    <div class="w-full flex flex-column justify-content-center" style="max-width: 60em;" >
+    <div class="w-full flex flex-column justify-content-center" style="max-width: 60rem;" >
       <div class="flex flex-column md:flex-row flex-wrap justify-content-around align-content-around">
         <div class="w-14rem m-1 md:mx-6 lg:mx-1">
           <label for="currencies" class="font-bold block"> Currency: </label>
@@ -48,137 +48,124 @@
       </Fieldset>
       <div class="w-full card align-self-center" >
         <PrimeChart ref="primaryChart" :chart-data="chartData"/>
-        <Button icon="pi pi-refresh" @click="primaryChart.reinit()" />
+        <!-- <Button icon="pi pi-refresh" @click="primaryChart.reinit()" /> -->
+      </div>
+      <div>
+        <Button label="Ad-Hoc Payments" icon="pi pi-external-link" @click="showAdHocPaymentsDialog = true" />
+        <Dialog class="w-11" v-model:visible="showAdHocPaymentsDialog" modal header="AD-HOC PAYMENTS" style="max-width: 60rem;" >
+          <table >
+            <thead>
+              <tr>
+                <th colspan="13">Enter any additional payments which you make into your bond account. Loans from it are entered as negative numbers.</th>
+              </tr>
+              <tr>
+                <th>Month:</th>
+                <th v-for="n in 12" :key="n">{{n}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="m in bondStore.finalYear + 1" :key="m">
+                <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
+                <td v-for="i in 12" :key="i">
+                  <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
+                  <input v-else type="number" v-model.lazy="bondStore.adHocPayments[(m-1)*12 + i - bondStore.dates[0].getMonth()]" placeholder="0" style="width: 5em;">
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Dialog>
+
+        <Button label="Interest Rates" icon="pi pi-external-link" @click="showInterestRatesDialog = true" />
+        <Dialog class="w-11" v-model:visible="showInterestRatesDialog" modal header="INTEREST RATES" style="max-width: 60rem;" >
+          <table>
+            <thead>
+              <tr>
+                <th colspan="13">Fill in the new interest rate in the month in which it changes.</th>
+              </tr>
+              <tr>
+                <th></th>
+                <th colspan="12">Months</th>
+              </tr>
+              <tr>
+                <th>Years</th>
+                <th v-for="n in 12" :key="n">{{n}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="m in bondStore.finalYear + 1" :key="m">
+                <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
+                <td v-for="i in 12" :key="i">
+                  <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
+                  <input v-else type="number" v-model.lazy="bondStore.adHocInterest[(m-1)*12 + i]" :placeholder="bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].annualInterest" style="width: 5em;">
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Dialog>
+
+        <Button label="Bond Repayments" icon="pi pi-external-link" @click="showBondPaymentsDialog = true" />
+        <Dialog class="w-11" v-model:visible="showBondPaymentsDialog" modal header="BOND REPAYMENTS" style="max-width: 60rem;" >
+          <table>
+            <thead>
+              <tr>
+                <th colspan="13">Fill in the new bond repayment amount in the month in which it changes.</th>
+              </tr>
+              <tr>
+                <th></th>
+                <th colspan="12">Months</th>
+              </tr>
+              <tr>
+                <th>Years</th>
+                <th v-for="n in 12" :key="n">{{n}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="m in bondStore.finalYear + 1" :key="m">
+                <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
+                <td v-for="i in 12" :key="i">
+                  <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
+                  <input v-else type="number" v-model.lazy="bondStore.adHocMonthlyPayments[(m-1)*12 + i]" :placeholder="bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].payment" style="width: 5em;">
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Dialog>
+
+        <Button label="Running Capital" icon="pi pi-external-link" @click="showRunningCapitalDialog = true" />
+        <Dialog class="w-11" v-model:visible="showRunningCapitalDialog" modal header="RUNNING CAPITAL" style="max-width: 60rem;" >
+          <table>
+            <thead>
+              <tr>
+                <th colspan="13">Figures represent the remaining capital at the end of each month.</th>
+              </tr>
+              <tr>
+                <th></th>
+                <th colspan="12">Months</th>
+              </tr>
+              <tr>
+                <th>Years</th>
+                <th v-for="n in 12" :key="n">{{n}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="m in bondStore.finalYear + 1" :key="m">
+                <td>{{ bondStore.dates[0].getFullYear() + m -1 }}</td>
+                <td v-for="i in 12" :key="i">
+                  <div v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" style="width: 7em; text-align: center;">-</div>
+                  <div v-else style="width: 7em;">{{bondStore.currency.symbol}}{{ currencyFormatter.format(bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].capital) }}</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Dialog>
       </div>
     </div>
   </div>
 
   
   <!-- _____________________________________________________________________________________________________________ -->
-  <div class="bg-primary md:bg-primary-reverse lg:bg-primary max-w-screen">Colours will invert during md size only.</div>
-  <Button icon="pi pi-question-circle" @click="buttonPress" />
-  <div>
-    <Button @click="adHocPaymentsDialog.showModal()"> Ad-Hoc Payments</Button>
-    <dialog ref="adHocPaymentsDialog">
-      <table >
-        <thead>
-          <tr>
-            <th colspan="13">AD HOC PAYMENTS</th>
-          </tr>
-          <tr>
-            <th colspan="13">Enter any additional payments which you make into your bond account. Loans from it are entered as negative numbers.</th>
-          </tr>
-          <tr>
-            <th></th>
-            <th v-for="n in 12" :key="n">{{n}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="m in bondStore.finalYear + 1" :key="m">
-            <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
-            <td v-for="i in 12" :key="i">
-              <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
-              <input v-else type="number" v-model.lazy="bondStore.adHocPayments[(m-1)*12 + i - bondStore.dates[0].getMonth()]" placeholder="0" style="width: 5em;">
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Button @click="adHocPaymentsDialog.close()">Close</Button>
-    </dialog>
-    <Button @click="interestRatesDialog.showModal()"> Interest Rates</Button>
-    <dialog ref="interestRatesDialog">
-      <table>
-        <thead>
-          <tr>
-            <th colspan="13">INTEREST RATES</th>
-          </tr>
-          <tr>
-            <th colspan="13">Fill in the new interest rate in the month in which it changes.</th>
-          </tr>
-          <tr>
-            <th></th>
-            <th colspan="12">Months</th>
-          </tr>
-          <tr>
-            <th>Years</th>
-            <th v-for="n in 12" :key="n">{{n}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="m in bondStore.finalYear + 1" :key="m">
-            <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
-            <td v-for="i in 12" :key="i">
-              <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
-              <input v-else type="number" v-model.lazy="bondStore.adHocInterest[(m-1)*12 + i]" :placeholder="bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].annualInterest" style="width: 5em;">
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Button @click="interestRatesDialog.close()">Close</Button>
-    </dialog>
-    <Button @click="bondPaymentsDialog.showModal()"> Bond Repayments</Button>
-    <dialog ref="bondPaymentsDialog">
-      <table>
-        <thead>
-          <tr>
-            <th colspan="13">BOND REPAYMENTS</th>
-          </tr>
-          <tr>
-            <th colspan="13">Fill in the new bond repayment amount in the month in which it changes.</th>
-          </tr>
-          <tr>
-            <th></th>
-            <th colspan="12">Months</th>
-          </tr>
-          <tr>
-            <th>Years</th>
-            <th v-for="n in 12" :key="n">{{n}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="m in bondStore.finalYear + 1" :key="m">
-            <td>{{ bondStore.dates[0].getFullYear() + m-1 }}</td>
-            <td v-for="i in 12" :key="i">
-              <input v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" type="number" disabled="true" style="width: 5em;">
-              <input v-else type="number" v-model.lazy="bondStore.adHocMonthlyPayments[(m-1)*12 + i]" :placeholder="bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].payment" style="width: 5em;">
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Button @click="bondPaymentsDialog.close()">Close</Button>
-    </dialog>
-    <Button @click="runningCapitalDialog.showModal()"> Running Capital</Button>
-    <dialog ref="runningCapitalDialog">
-      <table>
-        <thead>
-          <tr>
-            <th colspan="13">RUNNING CAPITAL</th>
-          </tr>
-          <tr>
-            <th colspan="13">Figures represent the remaining capital at the end of each month.</th>
-          </tr>
-          <tr>
-            <th></th>
-            <th colspan="12">Months</th>
-          </tr>
-          <tr>
-            <th>Years</th>
-            <th v-for="n in 12" :key="n">{{n}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="m in bondStore.finalYear + 1" :key="m">
-            <td>{{ bondStore.dates[0].getFullYear() + m -1 }}</td>
-            <td v-for="i in 12" :key="i">
-              <div v-if="(m < 2 && i < bondStore.dates[0].getMonth()+1) || (m == bondStore.finalYear + 1 && i > bondStore.dates[bondStore.finalYear*12].getMonth())" style="width: 7em; text-align: center;">-</div>
-              <div v-else style="width: 7em;">{{bondStore.currency.symbol}}{{ currencyFormatter.format(bondStore.runningCalcs[(m-1)*12 + i - bondStore.dates[0].getMonth()].capital) }}</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Button @click="runningCapitalDialog.close()">Close</Button>
-    </dialog>
-  </div>
+  <!-- <div class="bg-primary md:bg-primary-reverse lg:bg-primary max-w-screen">Colours will invert during md size only.</div> -->
+  <!-- <Button icon="pi pi-question-circle" @click="buttonPress" /> -->
   
 </template>
 
@@ -194,14 +181,16 @@ import Button from "primevue/button"
 import Dropdown from "primevue/dropdown"
 import Calendar from "primevue/calendar"
 import Fieldset from "primevue/fieldset"
+import Dialog from "primevue/dialog"
 
 const currencyFormatter = new Intl.NumberFormat("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })
 
-const adHocPaymentsDialog = ref()
-const interestRatesDialog = ref()
-const bondPaymentsDialog = ref()
-const runningCapitalDialog = ref()
 const primaryChart = ref()
+
+const showAdHocPaymentsDialog = ref(false)
+const showInterestRatesDialog = ref(false)
+const showBondPaymentsDialog = ref(false)
+const showRunningCapitalDialog = ref(false)
 
 onMounted(() => {
   window.addEventListener("resize", resizeHandler.execute)
@@ -286,9 +275,9 @@ const parseCalcs = () => {
 
 watchEffect(() => parseCalcs())
 
-const buttonPress = () => {
-  console.log(primaryChart)
-}
+// const buttonPress = () => {
+//   console.log(primaryChart)
+// }
 
 const actualPaymentInput = () => {
   bondStore.customPayments = true
