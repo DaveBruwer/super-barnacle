fb1de80518d9e603491578ccdba363e520e52453
DaveBruwer <davebruwer@gmail.com>
2024-01-07 22:17:25 +1300

    made BasicLoan3 the home route

--- src/router/index.js
+++ src/router/index.js
@@ -2,35 +2,35 @@
 import BasicLoan from "../views/BasicLoanView.vue"
 import BasicLoan2 from "../views/BasicLoan2View.vue"
 import BasicLoan3 from "../components/BasicLoan.vue"
-import TestPage from "../components/TestPage.vue"
+// import TestPage from "../components/TestPage.vue"
 import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: TestPage
+    component: BasicLoan3,
   },
   {
     path: "/basicloan",
     name: "BasicLoan",
-    component: BasicLoan
+    component: BasicLoan,
   },
   {
     path: "/basicloan2",
     name: "BasicLoan2",
-    component: BasicLoan2
+    component: BasicLoan2,
   },
   {
     path: "/basicloan3",
     name: "BasicLoan3",
-    component: BasicLoan3
-  }
+    component: BasicLoan3,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
