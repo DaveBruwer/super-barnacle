ce6854eaad217830fb5785e89e8dcd10b1f6c00c
Dave Bruwer <dave.bruwer@fphcare.co.nz>
2024-01-19 08:00:06 +1300

    applied max width to logo

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -2,7 +2,7 @@
   <div
     class="m-2 h-screen flex flex-column justify-content-center align-content-center align-items-center flex-wrap"
   >
-    <img alt="logo" src="/LoanSim_Logo1.svg" height="200" class="mr-2" />
+    <img alt="logo" src="/LoanSim_Logo1.svg" class="mr-2 w-full max-w-30rem" />
     <h1 class="w-max">Coming Soon!</h1>
     <h3 class="w-auto">
       This page is still under development and will be ready soon.
